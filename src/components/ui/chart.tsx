beb2b680e9b866ef7f6a3143329ba75eb114c356
sasha125588 <sashavolskiy11@gmail.com>
2024-12-10 23:28:35 +0200

    chart fix 2

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -70,7 +70,11 @@ ChartContainer.displayName = "Chart"
 const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
   const colorConfig = Object.entries(config).filter(
     /*eslint no-unused-vars: "error"*/
-    ([_, config]) => config.theme || config.color
+    ([_, config]) => {
+      config.theme || config.color
+      _ = _
+    }
+    
   )
 
   if (!colorConfig.length) {
